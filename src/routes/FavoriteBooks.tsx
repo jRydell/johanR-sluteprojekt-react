8f2dc89dc1ef3c40a3edad55b931fbaaaf3e7457
Johan Rydell <johannilsrydell89@gmail.com>
2024-05-04 18:40:44 +0200

    added favorite book list

--- src/routes/FavoriteBooks.tsx
+++ src/routes/FavoriteBooks.tsx
@@ -1,5 +1,30 @@
+import { useGlobalState } from "../hooks/useGlobalState";
+import { NavLink, Outlet } from "react-router-dom";
+
 const FavoriteBooks = () => {
-  return <div>Favorite Books</div>;
+  const { state } = useGlobalState();
+
+  return (
+    <>
+      <section>
+        <h2>Favorite Books</h2>
+        <ul>
+          {state.favoriteBooks.length ? (
+            state.favoriteBooks.map((book) => (
+              <li key={book.key}>
+                <NavLink to={`/book/${book.key}`}>{book.title}</NavLink>
+              </li>
+            ))
+          ) : (
+            <p>No Favorites</p>
+          )}
+        </ul>
+      </section>
+      <section>
+        <Outlet />
+      </section>
+    </>
+  );
 };
 
 export default FavoriteBooks;
