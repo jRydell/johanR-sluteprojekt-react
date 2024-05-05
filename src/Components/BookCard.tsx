9b2f80f5780f24757563ce780d71a9d8bf615c16
Johan Rydell <johannilsrydell89@gmail.com>
2024-05-05 21:20:36 +0200

    added some delete button styling

--- src/Components/BookCard.tsx
+++ src/Components/BookCard.tsx
@@ -64,25 +64,28 @@ const BookCard: React.FC<BookCardProps> = ({
           </button>
         </section>
       )}
-
-      {deleteFavoriteButton && (
-        <button
-          onClick={() =>
-            dispatch({ type: "REMOVE_FAVORITE_BOOK", payload: book })
-          }
-          className=""
-        >
-          Delete
-        </button>
-      )}
-      {deleteReadButton && (
-        <button
-          onClick={() => dispatch({ type: "REMOVE_READ_BOOK", payload: book })}
-          className=""
-        >
-          Delete
-        </button>
-      )}
+      <div className="flex justify-center">
+        {deleteFavoriteButton && (
+          <button
+            onClick={() =>
+              dispatch({ type: "REMOVE_FAVORITE_BOOK", payload: book })
+            }
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none active:bg-red-500 active:hover:bg-red-700 "
+          >
+            Delete
+          </button>
+        )}
+        {deleteReadButton && (
+          <button
+            onClick={() =>
+              dispatch({ type: "REMOVE_READ_BOOK", payload: book })
+            }
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none active:bg-red-500 active:hover:bg-red-700"
+          >
+            Delete
+          </button>
+        )}
+      </div>
     </section>
   );
 };
