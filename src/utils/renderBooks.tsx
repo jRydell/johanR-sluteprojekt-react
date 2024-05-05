70615111a5deed55809f012ca6b4981049831f0b
Johan Rydell <johannilsrydell89@gmail.com>
2024-05-05 10:35:32 +0200

    added deletButton parameter to renderBooks

--- src/utils/renderBooks.tsx
+++ src/utils/renderBooks.tsx
@@ -1,11 +1,19 @@
 import BookCard from "../Components/BookCard";
 import { Book } from "../types/types";
 
-export const renderBooks = (books: Book[], searchPageButtons: boolean) => {
+export const renderBooks = (
+  books: Book[],
+  searchPageButtons: boolean,
+  deleteButton: boolean
+) => {
   if (books.length) {
     return books.map((book) => (
       <li key={book.key}>
-        <BookCard book={book} searchPageButtons={searchPageButtons} />
+        <BookCard
+          book={book}
+          searchPageButtons={searchPageButtons}
+          deleteButton={deleteButton}
+        />
       </li>
     ));
   } else {
