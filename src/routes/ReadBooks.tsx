4bc91b804772694cf341579027b4557364d9bf34
Johan Rydell <johannilsrydell89@gmail.com>
2024-05-05 09:52:55 +0200

    added ReadBooks.tsx

--- src/routes/ReadBooks.tsx
+++ src/routes/ReadBooks.tsx
@@ -1,5 +1,20 @@
+import {useGlobalState} from "../hooks/useGlobalState";
+import { renderBookList } from "../utils/renderBooks";
+
 const ReadBooks = () => {
-  return <div>ReadBooks</div>;
+const {state} = useGlobalState();
+
+return (
+    <>
+      <section>
+        <h2 className="">Favorite Books</h2>
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {renderBookList(state.readBooks, false)}
+        </ul>
+      </section>
+    </>
+  );
+};
 };
 
 export default ReadBooks;
