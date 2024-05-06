ba7b4a3b05f42364d66ac8e6860b1bd8c9cf9b5e
Johan Rydell <johannilsrydell89@gmail.com>
2024-05-06 16:30:00 +0200

    add url object

--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -1,14 +1,16 @@
 import SearchComponent from "../Components/SearchComponent";
 
 const Search = () => {
+  const url = {
+    books: "https://openlibrary.org/search.json?title=",
+    authors: "https://openlibrary.org/search.json?author=",
+  };
+
   return (
     <>
       <section className="">
         <h2>Find books</h2>
-        <SearchComponent
-          placeholder={"Enter a book"}
-          url={"https://openlibrary.org/search.json?q="}
-        />
+        <SearchComponent placeholder={"Enter a book"} url={url.books} />
       </section>
     </>
   );
