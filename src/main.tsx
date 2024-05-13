46ac7d07d3323104a570183eaabd6f5f93e5458f
Johan Rydell <johannilsrydell89@gmail.com>
2024-05-13 14:25:27 +0200

    small fix

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import FavoriteBooks from "./routes/FavoriteBooks";
 import ReadBooks from "./routes/ReadBooks";
 import FavoriteAuthors from "./routes/FavoriteAuthors";
 import React from "react";
-import { GlobalStateProvider } from "./Components/GlobalStateProvider";
+import { GlobalStateProvider } from "./components/GlobalStateProvider";
 
 const router = createBrowserRouter([
   {
