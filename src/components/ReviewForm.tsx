3ad7a13beecbb3e0f0c70b5b564c97deadd0add6
Johan Rydell <johannilsrydell89@gmail.com>
2024-05-20 18:10:35 +0200

    fixed form and added validation

--- src/components/ReviewForm.tsx
+++ src/components/ReviewForm.tsx
@@ -15,67 +15,75 @@ const ReadBookForm = ({
   const review = useFormInput("");
   const numPages = useFormInput("");
 
-  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    if (parseInt(rating.value) < 1 || parseInt(rating.value) > 5) {
+      alert("Rating must be between 1 and 5.");
+      return;
+    }
+    if (parseInt(numPages.value) <= 0) {
+      alert("Number of pages must be greater than 0.");
+      return;
+    }
+
     dispatch({
       type: "ADD_READ_BOOK",
       payload: {
-        key: key,
-        title: title,
-        first_publish_year: first_publish_year,
-        author_name: author_name,
-        cover_i: cover_i,
+        key,
+        title,
+        first_publish_year,
+        author_name,
+        cover_i,
         rating: rating.value,
         review: review.value,
         numPages: numPages.value,
       },
     });
+
     setIsRead(!isRead);
     setIsOpen(!isOpen);
   };
+
   return (
-    <>
-      <article className="w-fit mt-5">
-        <form className="flex flex-col" onSubmit={handleSubmit}>
-          <>
-            <label htmlFor="review">Review:</label>
-            <textarea
-              className="border border-gray-300"
-              {...review}
-              id="review"
-              placeholder="Enter your review here..."
-              autoFocus
-            />
-          </>
-          <>
-            <label htmlFor="rating ">Rating:</label>
-            <input
-              className="border border-gray-300"
-              type="number"
-              {...rating}
-              id="rating"
-              placeholder="Enter your rating here..."
-            />
-          </>
-          <>
-            <label htmlFor="numPages">Number of pages:</label>
-            <input
-              className="border border-gray-300"
-              type="number"
-              {...numPages}
-              id="numPages"
-              placeholder="Enter number of pages here"
-            />
-          </>
-          <button
-            className="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none active:bg-red-500 active:hover:bg-red-700"
-            type="submit"
-          >
-            Add
-          </button>
-        </form>
-      </article>
-    </>
+    <form className="flex flex-col w-fit mt-5" onSubmit={handleSubmit}>
+      <label htmlFor="review">Review:</label>
+      <textarea
+        className="border border-gray-300"
+        {...review}
+        id="review"
+        placeholder="Enter your review here..."
+        autoFocus
+      />
+
+      <label htmlFor="rating">Rating:</label>
+      <input
+        className="border border-gray-300"
+        type="number"
+        {...rating}
+        id="rating"
+        placeholder="Enter your rating here..."
+        min="1"
+        max="5"
+      />
+
+      <label htmlFor="numPages">Number of pages:</label>
+      <input
+        className="border border-gray-300"
+        type="number"
+        {...numPages}
+        id="numPages"
+        placeholder="Enter number of pages here..."
+        min="1"
+      />
+
+      <button
+        className="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none active:bg-red-500 active:hover:bg-red-700"
+        type="submit"
+      >
+        Add
+      </button>
+    </form>
   );
 };
 
